31934027071e21f1eb2263e88e4726458ebadd78
vasilisa <vasilisaskv@gmail.com>
2020-03-02 08:47:50 +0000

    config change

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL='https://devnoise.osc-fr1.scalingo.io'
-export const API_URL='https://rlnoise.osc-fr1.scalingo.io'
+export const API_URL='https://devnoise.osc-fr1.scalingo.io'
+// export const API_URL='https://rlnoise.osc-fr1.scalingo.io'
